97bb353d12e640e3dfb888552d261405640df419
Dang <tonminhdang9@gmail.com>
2025-10-25 23:50:22 +0700

    Add project

---
+++ app/src/main/java/com/example/final_app/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.final_app;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.feature_login.presentation.LoginActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        startActivity(new Intent(this, LoginActivity.class));
+        finish();
+    }
+}
